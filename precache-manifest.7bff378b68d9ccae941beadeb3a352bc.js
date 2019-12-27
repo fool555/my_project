8a797cdf2cdb8e282bd3ab4df0838cc9b0ea8dc0
fool <fool@localhost.localdomain>
2019-12-28 02:15:10 +0500

    Updates

---
+++ precache-manifest.7bff378b68d9ccae941beadeb3a352bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fa080596d7006457a7beb3f2548c87",
+    "url": "/my_project/index.html"
+  },
+  {
+    "revision": "94b56441af55084efa65",
+    "url": "/my_project/static/css/main.1774f0ce.chunk.css"
+  },
+  {
+    "revision": "ff0178b2c039e839d26a",
+    "url": "/my_project/static/js/2.0a2f46cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_project/static/js/2.0a2f46cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94b56441af55084efa65",
+    "url": "/my_project/static/js/main.db8c1d4e.chunk.js"
+  },
+  {
+    "revision": "77aa31f913b1d6758cac",
+    "url": "/my_project/static/js/runtime-main.198e64af.js"
+  }
+]);
